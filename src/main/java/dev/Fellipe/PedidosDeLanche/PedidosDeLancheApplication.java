e32ee535ed88fe88d9e9edf85f330835a1232928
SmithBfr <fellipev72@gmail.com>
2026-04-15 21:21:57 -0300

    Primeiro commit

---
+++ src/main/java/dev/Fellipe/PedidosDeLanche/PedidosDeLancheApplication.java
@@ -0,0 +1,13 @@
+package dev.Fellipe.PedidosDeLanche;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedidosDeLancheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedidosDeLancheApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/Fellipe/PedidosDeLanche/PedidosDeLancheApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.Fellipe.PedidosDeLanche;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidosDeLancheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
